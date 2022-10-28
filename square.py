60b0727891eebf934e17300afd23fb78654aeee9
Jyotisikha <92041342+Jyotisikha@users.noreply.github.com>
2022-10-28 16:57:14 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,23 @@
+math.sqrt(x)
+
+Parameter: 
+x is any number such that x>=0 
+
+Returns:
+It returns the square root of the number 
+passed in the parameter. 
+
+# Python3 program to demonstrate the 
+# sqrt() method 
+  
+# import the math module 
+import math 
+  
+# print the square root of  0 
+print(math.sqrt(0)) 
+  
+# print the square root of 4
+print(math.sqrt(4)) 
+  
+# print the square root of 3.5
+print(math.sqrt(3.5)) 
